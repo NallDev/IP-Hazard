05387bb645315edadc82560e4d426df19167030f
AfrinaldiLibrary <afrinaldi190401@gmail.com>
2023-06-07 23:30:01 +0700

    FEAT : Add Machine Learning Model

--- app/src/main/java/com/example/iphazard/MainActivity.kt
+++ app/src/main/java/com/example/iphazard/MainActivity.kt
@@ -4,14 +4,20 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.iphazard.databinding.ActivityMainBinding
+import com.example.iphazard.ml.Model
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
@@ -35,6 +41,9 @@ class MainActivity : AppCompatActivity() {
                 Glide.with(this)
                     .load(image)
                     .into(binding.ivCamera)
+
+
+                outputGenerator(image)
             }
         }
 
@@ -56,6 +65,7 @@ class MainActivity : AppCompatActivity() {
         binding.ivCamera.setOnClickListener {
             openCamera()
         }
+
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -70,5 +80,53 @@ class MainActivity : AppCompatActivity() {
     private fun openCamera() {
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         takePictureLauncher.launch(takePictureIntent)
+    }
+
+    private fun outputGenerator(bitmap: Bitmap) {
+        val labels =
+            application.assets.open("labels.txt").bufferedReader().use { it.readText() }.split("\n")
+
+        val resized = Bitmap.createScaledBitmap(bitmap, 224, 224, true)
+        val model = Model.newInstance(this)
+
+        val tensorImage = TensorImage(DataType.FLOAT32)
+        tensorImage.load(resized)
+        val tbuffer = tensorImage
+
+//            var tbuffer = TensorImage.fromBitmap(resized)
+        val byteBuffer = tbuffer.buffer
+
+        Log.e("shape", tbuffer.buffer.toString())
+
+// Creates inputs for reference.
+        val inputFeature0 =
+            TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+        inputFeature0.loadBuffer(byteBuffer)
+
+// Runs model inference and gets result.
+        val outputs = model.process(inputFeature0)
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+        Log.e("shape", outputFeature0.floatArray.toString())
+        val max = getMax(outputFeature0.floatArray)
+//            Log.e("shape", max.toString())
+        Log.e("shape", inputFeature0.buffer.toString())
+
+        binding.tvPredict.text = labels[max]
+
+        model.close()
+    }
+
+    private fun getMax(arr: FloatArray): Int {
+        var ind = 0
+        var min = 0.0f
+
+        for (i in 0..8) {
+            if (arr[i] > min) {
+                min = arr[i]
+                ind = i
+            }
+        }
+        return ind
     }
 }
